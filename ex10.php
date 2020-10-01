abb5b2707b1d3abd78ccca65b9be605452215a10
GeethOnion <geethmaka@gmail.com>
2020-10-01 22:21:23 +0530

    Added ex10

---
+++ ex10.php
@@ -0,0 +1,16 @@
+<?php
+//This is an exercise about how to connect to a database using php
+
+$servername = "localhost"; 
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
