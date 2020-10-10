2a36c5204b1dbf396a18230ab877e7dc7ad61410
GeethOnion <geethmaka@gmail.com>
2020-10-10 21:33:15 +0530

    Added ex11

---
+++ ex11.php
@@ -0,0 +1,11 @@
+<?php
+
+//This is an exercise about how to use date and time on php
+//syntax - date(format,timestamp)
+
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+
+?>
